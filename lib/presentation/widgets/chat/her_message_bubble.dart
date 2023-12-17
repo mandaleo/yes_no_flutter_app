9a8b2d9e6d0e5b3f1c5426d85f2939f9b9164143
Manuel Martinez Gomez <manuel.martinez@mandaleo.es>
2023-12-17 11:30:23 +0100

    Upload

---
+++ lib/presentation/widgets/chat/her_message_bubble.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:yes_no_app/domain/entities/message.dart';
+
+class HerMessageBuble extends StatelessWidget {
+  final Message message;
+  const HerMessageBuble({super.key, required this.message});
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Container(
+          decoration: BoxDecoration(
+              color: colors.secondary, borderRadius: BorderRadius.circular(20)),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child: Text(
+              message.text,
+              style: const TextStyle(color: Colors.white),
+            ),
+          ),
+        ),
+        const SizedBox(height: 5),
+        _ImageBubble(imageUrl: message.imageUrl),
+        const SizedBox(height: 10),
+      ],
+    );
+  }
+}
+
+class _ImageBubble extends StatelessWidget {
+  final String? imageUrl;
+
+  const _ImageBubble({required this.imageUrl});
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return ClipRRect(
+        borderRadius: BorderRadius.circular(20),
+        child: Image.network( imageUrl ??
+          'https://yesno.wtf/assets/yes/6-304e564038051dab8a5aa43156cdc20d.gif',
+          width: size.width * 0.7,
+          height: 150,
+          fit: BoxFit.cover,
+          loadingBuilder: (context, child, loadingProgress) {
+            if (loadingProgress == null) return child;
+            return Container(
+              width: size.width * 0.7,
+              height: 150,
+              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+              child: const Text('Mi amor está enviando una imagen'),
+            );
+          },
+        ));
+  }
+}
